b23fb9a03a70332402308dd59bc2ee6b80488816
NDmitrieva <al_amanecer@mail.ru>
2021-10-02 15:55:07 +0300

    Miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 65;
+        int count = 20;
+        int miles = price / count;
+
+        System.out.println("Начисленные мили: " + miles);
+
+    }
+}
